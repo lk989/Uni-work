b37fe82e82005382592c4abc8b5047b7c12aa90f
lk989 <96926388+lk989@users.noreply.github.com>
2023-04-15 08:39:57 +0300

    Delete JavaFX labs/EventDriven/EventDriven directory

--- JavaFX labs/EventDriven/EventDriven/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.eventdriven {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.eventdriven to javafx.fxml;
-    exports com.example.eventdriven;
-}
